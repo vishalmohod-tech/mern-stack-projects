3954655c39cb3905c76d2e1796091045f6fff452
Vishal Mohod <vishalmohod.tech@gmail.com>
2025-11-01 11:05:57 +0530

    Add files via upload

---
+++ Day 79 Project-52/visitor-count/server.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" || req.url === "/visitor.html") {
+    fs.readFile(path.join(__dirname, "visitor.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/visitor.css") {
+    fs.readFile(path.join(__dirname, "visitor.css"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    });
+  } else if (req.url === "/visitor.js") {
+    fs.readFile(path.join(__dirname, "visitor.js"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
